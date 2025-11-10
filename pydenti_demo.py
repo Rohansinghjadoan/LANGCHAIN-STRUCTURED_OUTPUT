1a48cd61b42dc65596ad621763164c5debcde8be
rohan <rohansinghjadoan@gmail.com>
2025-11-11 02:53:48 +0530

    with structured_output

--- pydenti_demo.py
+++ pydenti_demo.py
@@ -1,10 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr,Field
 from typing import Optional
 
 class Student(BaseModel):
-    name:str='nitish'
+    name:str='nitish' # default value 
     age:Optional[int]=None
+    email:EmailStr
+    cgpa:float=Field(gt=0,lt=10,default=5,description='a decimal valuer representing the cgpa of the student')
 
-new_student={}
+
+new_student={'age':'32','email':'abc@gmail.com'}
 Student=Student(**new_student)
-print(Student.name)
+student_dict= dict(Student)
+print(student_dict)
+
+
