f568918880f3550a460c33120ce5a6b848e90fd6
rohan <rohansinghjadoan@gmail.com>
2025-11-12 23:34:28 +0530

    output parser

---
+++ Output_Parsers/pydanticOutputparser.py
@@ -0,0 +1,31 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation"
+)
+model=ChatHuggingFace(llm=llm)
+class Person(BaseModel):
+    name:str=Field(description='Name of the person')
+    age:int=Field(description='Age of the person')
+    city:str=Field(description='Name of the city the person belongs to ')
+paser=PydanticOutputParser(pydantic_object=Person)
+
+template=PromptTemplate(
+    template='Generate the name ,age ,and city of a fictional {place} person \n{format_instruction}',
+    input_variables=['place'],
+    partial_variables={'format_instruction': paser.get_format_instructions()}
+)
+
+chain=template| model| paser
+final_result=chain.invoke({'place':'indian'})
+print(final_result)
+
+
+    
